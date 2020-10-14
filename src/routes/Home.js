a2d354f7af959f6f88b0b1ac7ed1a5379e14fc67
mufaddalnaya <mufaddal.naya@gmail.com>
2020-10-14 17:25:40 +0530

    New Authentication ALT TRY 2

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -4,71 +4,66 @@ import { connect } from 'react-redux'
 import { CognitoUser, AuthenticationDetails } from 'amazon-cognito-identity-js'
 import UserPool from './UserPool'
 
-const mapStateToProps = state => {
-  return { session: state.session }
-}
-
-const onSubmit = event => {
-  event.preventDefault();
-
-  const user = new CognitoUser({
-    Username: email,
-    Pool: UserPool
-  })
-
-  const authDetails = new AuthenticationDetails({
-    Username: email,
-    Password: password
-  })
-
-  user.authenticateUser(authDetails, {
-    onSuccess: data => {
-      console.log(data)
-    },
-
-    onFailure: err => {
-      console.log(err)
-    }, 
-
-    newPasswordRequired: data=> {
-      console.log('newPasswordRequired: ', data)
-    }
-
-  })
 
 
-}
+export default () => {
 
-const [email, setEmail] = useState('');
-const [password, setPassword] = useState('');
-
-
-class Home extends Component {
-  constructor (props) {
-    super(props)
-    console.log("session: "+this.props.session.isLoggedIn)
-    this.state = { apiStatus: 'Not called' }
+  const mapStateToProps = state => {
+    return { session: state.session }
   }
-  render () {
-    return (
-      <div className="Home">
-        <div>Hello</div>
-        <header className="Home-header">
-          <from onSubmit={onSubmit}>
-            <input 
-              value={email}
-              onChange={event => setEmail(event.target.value)}
-            />
-            <input 
-              value={password}
-              onChange={event => setPassword(event.target.value)}
-            />
-            <button type='submit'> Login </button>
-          </from>
-        </header>
-      </div>
-    )
-  }
-}
 
-export default connect(mapStateToProps)(Home)
+  const onSubmit = event => {
+    // event.preventDefault();
+
+    console.log('Inside')
+    const user = new CognitoUser({
+      Username: email,
+      Pool: UserPool
+    })
+
+    const authDetails = new AuthenticationDetails({
+      Username: email,
+      Password: password
+    })
+
+    user.authenticateUser(authDetails, {
+      onSuccess: data => {
+        console.log(data)
+      },
+
+      onFailure: err => {
+        console.log(err)
+      }, 
+
+      newPasswordRequired: data=> {
+        console.log('newPasswordRequired: ', data)
+      }
+
+    })
+
+
+  }
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+
+  return (
+    <div className="Home">
+      <div>Hello</div>
+        <from onSubmit={onSubmit}>
+          <input 
+            value={email}
+            onChange={event => setEmail(event.target.value)}
+          />
+          <input 
+            value={password}
+            onChange={event => setPassword(event.target.value)}
+          />
+          <button type='submit'> Login </button>
+        </from>
+    </div>
+  )
+}
+  
+// export default connect(mapStateToProps)(Home)
